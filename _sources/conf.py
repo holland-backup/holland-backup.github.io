ef1496f44dab3576a147a16541a9a8cb707792af
Steven Soulen <steven.soulen@rackspace.com>
2018-07-27 10:11:08 -0500

    Add version number

--- _sources/conf.py
+++ _sources/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Holland Core Team'
 author = 'Holland Core Team'
 
 # The short X.Y version
-version = ''
+version = '1.1.0'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
