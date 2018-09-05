54e04f8399bfbff8929374a4c46fbb848d03b28a
Steven Soulen <steven.soulen@rackspace.com>
2018-09-05 13:56:38 -0500

    Remove mysqldump from compression.txt, and try to make it clear what pgdump plugin is doing

--- _sources/conf.py
+++ _sources/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Holland Core Team'
 author = 'Holland Core Team'
 
 # The short X.Y version
-version = '1.1.3'
+version = '1.1.4'
 # The full version, including alpha/beta/rc tags
-release = '1.1.3'
+release = '1.1.4'
 
 
 # -- General configuration ---------------------------------------------------
