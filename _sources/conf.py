87b18c0ed62926ec17b3a43df469fcc3cacf54c3
Steven Soulen <steven.soulen@rackspace.com>
2018-11-07 10:32:42 -0600

    Add doc for new mysqldump arg_per_database function

--- _sources/conf.py
+++ _sources/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Holland Core Team'
 author = 'Holland Core Team'
 
 # The short X.Y version
-version = '1.1.8'
+version = '1.1.9'
 # The full version, including alpha/beta/rc tags
-release = '1.1.8'
+release = '1.1.9'
 
 
 # -- General configuration ---------------------------------------------------
