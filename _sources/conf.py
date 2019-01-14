e22d9e9a9977b14dddd10104609d9d3f8aed94dd
Steven Soulen <steven.soulen@rackspace.com>
2019-01-14 17:08:14 -0600

    bump version

--- _sources/conf.py
+++ _sources/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Holland Core Team'
 author = 'Holland Core Team'
 
 # The short X.Y version
-version = '1.1.9'
+version = '1.1.10'
 # The full version, including alpha/beta/rc tags
-release = '1.1.9'
+release = '1.1.10'
 
 
 # -- General configuration ---------------------------------------------------
