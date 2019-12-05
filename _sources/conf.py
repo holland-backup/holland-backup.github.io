ae5bbc9be4e6a598deef2781d8288b8f043ec443
Steven Soulen <steven.soulen@rackspace.com>
2019-12-05 10:39:42 -0600

    Update docs

--- _sources/conf.py
+++ _sources/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Holland Core Team'
 author = 'Holland Core Team'
 
 # The short X.Y version
-version = '1.1.18'
+version = '1.1.20'
 # The full version, including alpha/beta/rc tags
-release = '1.1.18'
+release = '1.1.20'
 
 
 # -- General configuration ---------------------------------------------------
